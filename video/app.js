44af550386c2ec01ced9ad977011e976db0d222d
Derryck <oseiagyemanb@gmail.com>
2023-01-17 05:00:29 +0100

    update file with preloader

--- video/app.js
+++ video/app.js
@@ -2,11 +2,18 @@ const btn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
 
 btn.addEventListener('click', () => {
-    if (!btn.classList.contains('silde')) {
+    if (!btn.classList.contains('slide')) {
         btn.classList.add('slide');
         video.pause();
     } else {
         btn.classList.remove('slide');
         video.play();
     }
+});
+
+//adding of preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+    preloader.classList.add('hide-preloader');
 });
