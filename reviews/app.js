81401fd9654ca1ed09d4201bb158b8bb1d42dca3
Derryck <oseiagyemanb@gmail.com>
2023-01-21 21:42:54 +0100

    modification of app.js file with lesser code lines

--- reviews/app.js
+++ reviews/app.js
@@ -1,34 +1,5 @@
 
-const reviews = [
-    {
-        id: 1,
-        name: "enoch antoei",
-        job: "web developer",
-        pic: "./enoch.jpg",
-        text: "The staff was great. The receptionists were very helpful and answered all our questions. The room was clean and bright, and room service was always on time. Will be coming back!",
-    },
-    {
-        id: 2,
-        name: "keziah amber",
-        job: "ux designer",
-        pic: "./keziah.jpg",
-        text: "The tool X has really automated some of our company's processes. We now spend less time doing manual work. It's making [problem] very easy for us. Thanks to its scheduling feature.",
-    },
-    {
-        id: 3,
-        name: "fred davies",
-        job: "consultant",
-        pic: "./fred.jpg",
-        text: "I got a pair of boots from store X and I'm very satisfied. They are high-quality and worth the money. The store also offered free shipping at that price so that's a plus",
-    },
-    {
-        id: 4,
-        name: "susan smith",
-        job: "secretary",
-        pic: "./susan.jpg",
-        text: "My husband and I went for dinner in restaurant X and really enjoyed the atmosphere. The food was fresh and delicious and the best part was that the chef sent us a dessert.",
-    }
-];
+import { reviews } from "./data.js"; 
 
 const img = document.getElementById('person-img');
 const author = document.getElementById('author');
@@ -63,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function () {
 //show the individual based on item
 function showIndvidual() {
     const item = reviews[currentItem];
-    console.log(item)
+    // console.log(item)
     img.src = item.pic;
     author.textContent = item.name;
     job.textContent = item.job;
@@ -92,7 +63,5 @@ prevBtn.addEventListener('click', function(){
 // random button
 randomBtn.addEventListener('click', () => {
 currentItem =  Math.floor(Math.random() * reviews.length);
-
 showIndvidual();
-
 });
