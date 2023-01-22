a2e29c9fb2ef76b1874e01e7642a08ca76aba837
Derryck <oseiagyemanb@gmail.com>
2023-01-22 21:57:10 +0100

    modication of app file with headingColor

--- color-fliper/app.js
+++ color-fliper/app.js
@@ -1,7 +1,9 @@
-const colors = ['blue', 'green', 'rgba(122,75,189)', '#f15025'] //these represent that we are going to have with different types.
+//these represent the colors that we are going to have with different types.
+const colors = ['blue', 'green', 'rgba(122,75,189)', 'hsl(90, 100%, 50%)', '#f15025', '#14c7e6', 'orange']
 
 const color = document.querySelector('.color');
 const btn = document.getElementById('btn');
+const headingColor = document.querySelector('.heading');
 
 btn.addEventListener('click', () => {
     //get random number between 0 - 3;
@@ -9,7 +11,8 @@ btn.addEventListener('click', () => {
     console.log(randomNumber)
 
     document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber]
+    color.textContent = colors[randomNumber];
+    headingColor.style.color = colors[randomNumber];
 })
 
 //this function will generate random numbers from 0 - 3 which correspond to the colors array
