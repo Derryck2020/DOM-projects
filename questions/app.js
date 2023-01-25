d07398d1ff17b8905530924290d46583e156eb37
Derryck <oseiagyemanb@gmail.com>
2023-01-26 00:09:58 +0100

    update of app.js

--- questions/app.js
+++ questions/app.js
@@ -1,23 +1,30 @@
-const questions = document.querySelectorAll('.question');
+//const questions = document.querySelectorAll('.question');
+
+// questions.forEach(function (params) {
+//     // console.log(params);
+//     const btn = params.querySelector('.question-btn'); //we didnt use document on the querySelector here, 
+//     //becos we dont need access to the whole document, we want to access only the article 
+//     // console.log(btn);
+//     btn.addEventListener('click', () => {
+
+//         //This function will prevent viewing another question when another one is opened.
+//         questions.forEach((item) => {
+//             if(item !== params){
+//                 item.classList.remove('show-text');
+//             }
+//         });
+
+//         params.classList.toggle('show-text'); //This was done first 
+//     });
+// });
 
 
-questions.forEach(function (params) {
-    // console.log(params);
-    const btn = params.querySelector('.question-btn'); //we didnt use document on the querySelector here, 
-    //becos we dont need access to the whole document, we want to access only the article 
-    // console.log(btn);
-    btn.addEventListener('click', () => {
+// METHOD II - Traversing the DOM (uisng selectors inside the element)
+const btns = document.querySelectorAll(".question-btn");
 
-        //This function will prevent viewing another question when another one is opened.
-        questions.forEach((item) => {
-            if(item !== params){
-                item.classList.remove('show-text');
-            }
-        });
-
-        params.classList.toggle('show-text'); //This was done first 
+btns.forEach(function (btn){
+    btn.addEventListener('click', function(e){
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
     });
 });
-
-
-//METHOD II - Using Traversing the DOM
