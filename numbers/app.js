d9412b660b3e3f14b0516c068142bbd8b8ec9160
Derryck <oseiagyemanb@gmail.com>
2023-02-14 05:02:06 +0100

    initial commit of files

---
+++ numbers/app.js
@@ -0,0 +1,22 @@
+const elements = [...document.querySelectorAll(".number")];
+
+const updateCount = (el) => {
+   const value = parseInt(el.dataset.value);
+   const increment = Math.ceil(value / 1000);
+   let initialValue = 0;
+
+   const increaseCount = setInterval(() => {
+      initialValue += increment;
+
+      if (initialValue > value) {
+         el.textContent = `${value}+`;
+         clearInterval(increaseCount);
+         return;
+      }
+      el.textContent = `${initialValue}+`;
+   }, 1);
+};
+
+elements.forEach((element) => {
+   updateCount(element);
+});
